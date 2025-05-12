3e6b3cfbe6f21bbcdbafaeb9d199872e1132a6e0
schollenkopf <nelson.becker@web.de>
2025-05-12 14:52:30 +0200

    fix config

--- mask3d/__init__.py
+++ mask3d/__init__.py
@@ -72,9 +72,10 @@ def get_model(checkpoint_path=None, dataset_name="scannet200"):
         cfg.general.dbscan_eps = 0.95
         cfg.general.export_threshold = 0.001
 
-        cfg.data.num_labels = 1
+        cfg.data.num_labels = 13
         cfg.data.add_colors = False
         cfg.data.add_normals = False
+        cfg.data.in_channels = 3
         cfg.data.test_mode = "test"
 
         cfg.model.num_queries = 150
