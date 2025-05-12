be4f265c297871bcac8a0f1b438071f4a215b0df
schollenkopf <nelson.becker@web.de>
2025-05-12 23:14:25 +0200

    to double

--- mask3d/__init__.py
+++ mask3d/__init__.py
@@ -167,7 +167,7 @@ def prepare_data(mesh, device):
 
     return (
         data,
-        torch.from_numpy(sample_coordinates),
+        torch.from_numpy(sample_coordinates).double(),
         colors,
         features,
         unique_map,
