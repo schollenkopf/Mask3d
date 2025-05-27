9bd83ff7479ad06bedad90be638f710c0c2e1e16
schollenkopf <nelson.becker@web.de>
2025-05-27 09:45:00 +0200

    less val

--- run_on_hpc.sh
+++ run_on_hpc.sh
@@ -93,7 +93,7 @@ python main_instance_segmentation.py \
   data.num_labels=4 \
   general.area=${CURR_AREA} \
   model.num_queries=${CURR_QUERY} \
-  trainer.check_val_every_n_epoch=1 \
+  trainer.check_val_every_n_epoch=15 \
 #   general.checkpoint="../checkpoints/area3_from_scratch.ckpt" \
 #   optimizer.lr=0.00001
 #   general.topk_per_image=${CURR_TOPK} \
