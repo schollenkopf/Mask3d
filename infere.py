c758c168f38e20d233128f20c1c6bc37f683b2f3
schollenkopf <nelson.becker@web.de>
2025-05-30 12:21:37 +0200

    infere

--- infere.py
+++ infere.py
@@ -10,7 +10,7 @@ import torch
 import os
 
 print("getting model")
-model = get_model("checkpoints/last-epoch230its.ckpt", "s3dis")
+model = get_model("checkpoints/last-epoch530its.ckpt", "s3dis")
 model.eval()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
